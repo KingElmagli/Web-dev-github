f6a882dad2bd6b0e26461827065176eb06820c78
KingElmagli <elmaglimostafa@gmail.com>
2024-06-13 00:31:49 +0100

    day30

---
+++ day30/app.js
@@ -0,0 +1,9 @@
+let age = 20;
+let username = "Mustafa";
+let hobbies = ["reading", "praying", "programming", "going to gym"];
+let job = {
+  title: "Developer"
+  place: "NewYork"
+  salary: 50000;
+};
+alert(hobbies[0]);
