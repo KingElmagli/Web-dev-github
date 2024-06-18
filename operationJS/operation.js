a75dcfefdcca6f928bbfb1fb33081304a35c68e3
Mustafa Osman <elmaglimostafa@gmail.com>
2024-06-18 16:42:26 +0100

    Added new files and JavaScript info

---
+++ operationJS/operation.js
@@ -0,0 +1,11 @@
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(11 % 2);
+
+console.log("Mustafa " + "Osman");
+
+let userName = "Mustafa";
+console.log(userName.length);
+console.log(userName.toUpperCase);
