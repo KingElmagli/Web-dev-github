6ad4f98914515986cdce2a3cc008a66a0da2aa9c
KingElmagli <elmaglimostafa@gmail.com>
2024-06-27 23:36:45 +0100

    day33

---
+++ day33/DOM/app.js
@@ -0,0 +1,11 @@
+//console.log(window.document);
+//console.log(document);this and the above do the same thing.
+
+//console.dir(document);
+
+document.body.children[1].children[0].href = "https://google.com"; // so in this code(the .href="")we're changing the path inwhich the link is leading us to which google.com and it was basically just our local development server, all the dots are basically just the paths in javascript.
+//the above indented code
+//the defer attribute
+//soultions to the error
+
+//DOM (Document Object Model)
