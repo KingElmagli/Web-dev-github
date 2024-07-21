41d7eb9366de185e4c9443a7a1bb901ccfb72f01
KingElmagli <elmaglimostafa@gmail.com>
2024-07-21 15:18:49 +0100

    day41

---
+++ day41/tic-tac-toe project/scripts/config.js
@@ -0,0 +1,14 @@
+function openPlayerConfig() {
+  playerConfigOverlayElment.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function openPlayerConfig() {
+  playerConfigOverlayElment.style.display = "none";
+  backdropElement.style.display = "none";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const form
+}
